c921bc20a373f2bd3b6a8ad36655b45bfca813e2
Ashish Kumar Sathish <asathis2@dcl-l520-22.ews.illinois.edu>
2017-04-19 14:23:08 -0500

    checkout test

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-
+#test2
 s.listen(5)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
